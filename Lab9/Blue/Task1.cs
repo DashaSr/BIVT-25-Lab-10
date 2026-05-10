ec5c5c02ec23089d286c96df465af733d518f836
DashaSr <sergeevada07@mail.ru>
2026-05-11 00:10:09 +0300

    Update Task1.cs

--- Lab9/Blue/Task1.cs
+++ Lab9/Blue/Task1.cs
@@ -1,59 +1,168 @@
-﻿﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+namespace Lab9.Blue;
 
-namespace Lab9.Blue
+public class Task1 : Blue
 {
-    public class Task1: Blue
+    private string[] output;
+
+    public string[] Output
     {
-        private string[] _output = Array.Empty<string>();
-        public string[] Output => (string[])_output.Clone();
-        public Task1(string input) : base(input)
+        get
         {
-            
+            return output;
         }
-        public override void Review()
+        
+        private set
         {
-            string summ =null ;
-            string[] words = Input.Split(' ');
-            
-            for (int i = 0; i < words.Length; i++)
+            output = value;
+        }
+    }
+    
+    public Task1(string text) : base(text)
+    {
+        Output = new string[0];
+        Review();
+    }
+
+    private bool IsSpace(char c)
+    {
+        return c == ' ';
+    }
+    
+    public override void Review()
+    {
+        string text = Input;
+        if (text == null || text.Length == 0)
+        {
+            Output = new string[0];
+            return;
+        }
+        
+        int linesCount = 0;
+        int currentLineLen = 0;
+        int i = 0;
+        while (i < text.Length)
+        {
+            while (i < text.Length && IsSpace(text[i]))
             {
-                if (summ == null)
+                i++;
+            }
+
+            if (i >= text.Length)
+            {
+                return;
+            }
+
+            int wordStart = i;
+            while (i < text.Length && !IsSpace(text[i]))
+            {
+                i++;
+            }
+            int wordLen = i - wordStart;
+
+            if (currentLineLen == 0)
+            {
+                currentLineLen = wordLen;
+            }
+            else
+            {
+                if (currentLineLen + 1 + wordLen <= 50)
                 {
-                    summ = words[i]; 
-                }
-                else if (summ.Length + words[i].Length + 1 > 50)
-                {
-                    Array.Resize(ref _output, _output.Length + 1);
-                    _output[^1] = summ;
-                    summ = null;
-                    summ = words[i];
-                }
-                else if (summ.Length + words[i].Length + 1 == 50)
-                {
-                    Array.Resize(ref _output, _output.Length + 1);
-                    _output[^1] = summ + ' ' + words[i];
-                    summ = null;
+                    currentLineLen = currentLineLen + 1 + wordLen;
                 }
                 else
                 {
-                    summ = summ + ' ' + words[i];
-                }
-                if (i == words.Length - 1)
-                {
-                    Array.Resize(ref _output, _output.Length + 1);
-                    _output[^1] = summ;
-                    summ = null;
+                    linesCount++;
+                    currentLineLen = wordLen;
                 }
             }
+        }
 
-        }
-        public override string ToString()
+        if (currentLineLen > 0)
         {
-            return string.Join(Environment.NewLine, _output);
+            linesCount++;
         }
+        
+        string[] result = new string[linesCount];
+        int lineIndex = 0;
+        currentLineLen = 0;
+        char[] lineBuffer = new char[50];
+        int bufferPos = 0;
+        i = 0;
+        while (i < text.Length)
+        {
+            while (i < text.Length && IsSpace(text[i]))
+            {
+                i++;
+            }
+
+            if (i >= text.Length)
+            {
+                return;
+            }
+
+            int wordStart = i;
+            while (i < text.Length && !IsSpace(text[i]))
+            {
+                i++;
+            }
+            int wordLen = i - wordStart;
+
+            if (bufferPos == 0)
+            {
+                for (int j = 0; j < wordLen; j++)
+                {
+                    lineBuffer[bufferPos++] = text[wordStart + j];
+                }
+                currentLineLen = wordLen;
+            }
+            else
+            {
+                if (currentLineLen + 1 + wordLen <= 50)
+                {
+                    lineBuffer[bufferPos++] = ' ';
+                    for (int j = 0; j < wordLen; j++)
+                    {
+                        lineBuffer[bufferPos++] = text[wordStart + j];
+                    }
+                    currentLineLen = currentLineLen + 1 + wordLen;
+                }
+                else
+                {
+                    result[lineIndex] = new string(lineBuffer, 0, bufferPos);
+                    lineIndex++;
+                    bufferPos = 0;
+                    for (int j = 0; j < wordLen; j++)
+                    {
+                        lineBuffer[bufferPos++] = text[wordStart + j];
+                    }
+                    currentLineLen = wordLen;
+                }
+            }
+        }
+        if (bufferPos > 0)
+        {
+            result[lineIndex] = new string(lineBuffer, 0, bufferPos);
+        }
+
+        Output = result;
+    }
+
+    public override string ToString()
+    {
+        if (Output == null || Output.Length == 0)
+        {
+            return "";
+        }
+        
+        string result = "";
+        for (int i = 0; i < Output.Length; i++)
+        {
+            result += Output[i];
+            if (i != Output.Length - 1)
+            {
+                result += "\n";
+            }
+        }
+        return result;
     }
 }
